b768d4427460d18479d4d8a1efcc1b2f5e033a68
ashrafidoost <reza.ashrafidoost@gmail.com>
2026-01-21 07:45:43 -0400

    Some modifications & Sql query prc

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q, F
 from store.models import Product
 
 # Create your views here.
@@ -11,8 +13,10 @@ def first_page(request):
     return HttpResponse('This is the first page of the Store Front')
 
 def say_hello(request):
-    query_set = Product.objects.get(pk=1)
 
-    print(query_set)
+    product = Product.objects.order_by('unit_price')
+    #product = Product.objects.latest('unit_price')
 
-    return render(request, 'hello.html', {'name': 'Reza'})
+    print(product)
+
+    return render(request, 'hello.html', {'name': 'Reza', 'products': product})
