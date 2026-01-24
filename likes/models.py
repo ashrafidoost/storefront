0b1cc71946610b6d3a0904e4e869d1a2636c9e09
ashrafidoost <reza.ashrafidoost@gmail.com>
2026-01-24 10:53:22 -0400

    Revised Database Tables - Likes - Tags

--- likes/models.py
+++ likes/models.py
@@ -1,13 +1,12 @@
+
 from django.db import models
+from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
-# Create your models here.
-class Tag(models.Model):
-    label = models.CharField(max_length=255)
 
-class TaggedItem(models.Model):
+class LikedItem(models.Model):
     # What tage applied to what object
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     # Type (product, video, article)
     # ID
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q, F
-from store.models import Product
+from django.db.models import Q, F, Func
+from django.db.models.functions import Concat
+from django.db.models import Avg, Max, Min, Count, Sum
+from django.db.models import Value, ExpressionWrapper, DecimalField
+from store.models import Product, OrderItem, Customer, Order
 
 # Create your views here.
 #----------------------------- VIEWS (Mosh)
@@ -14,9 +17,21 @@ def first_page(request):
 
 def say_hello(request):
 
-    product = Product.objects.order_by('unit_price')
-    #product = Product.objects.latest('unit_price')
+    #query_set = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20) & ~Q(last_update__year__lt=2021))
+    #query_set = list(Product.objects.all().order_by('unit_price').filter(last_update__year__lt=2021))[5:15]
+    #query_set = Product.objects.values('id', 'title', 'collection__title')
+    #query_set = Product.objects.filter(id__in=OrderItem.objects.values('product__id').distinct()).order_by('id', '-title')
+    #query_set = Product.objects.prefetch_related('promotions').select_related('collection').all()
+    #query_set = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+    #query_set = Product.objects.filter(collection__id=3).aggregate(cnt=Count('id'), maximum=Max('unit_price'), minimum=Min('unit_price'), summon=Sum('unit_price'))
+    #query_set = Customer.objects.annotate(is_new=Value(True), New_ID=F('id')+10, FullName=Func(F('first_name'), Value(' '), F('last_name'), function='concat'))
+    #query_set = Customer.objects.annotate(FullName=Concat('first_name', Value(' '), 'last_name'))
+    #query_set = Customer.objects.annotate(orders_count=Count('order'))
 
-    print(product)
+    discounted_price = ExpressionWrapper(F('unit_price')*0.8, output_field=DecimalField(max_digits=10, decimal_places=2))
+    query_set = Product.objects.annotate(discounted_price = discounted_price)
 
-    return render(request, 'hello.html', {'name': 'Reza', 'products': product})
+
+    #print(query_set)
+
+    return render(request, 'hello.html', {'name': 'Reza', 'products': query_set})
